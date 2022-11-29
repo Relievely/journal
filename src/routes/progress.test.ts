2072a69351946871a9fb07d918e9b6baf2616cc4
Marcel Dominic Bandowski <marcel.bandowski@dextermedia.net>
2022-11-29 18:55:51 +0100

    Merged progress.test.ts contents

--- src/routes/progress.test.ts
+++ src/routes/progress.test.ts
@@ -31,81 +31,82 @@ describe("Progress routes", () => {
     });
 
     it("should return the project object with the id 1", async () => {
-    describe("should handle item", () => {
-        let newID: number | bigint = 0;
+        describe("should handle item", () => {
+            let newID: number | bigint = 0;
 
-        it("should create new note item", async () => {
-            await requestWithSuperTest
-                .post("/progress")
-                .send({creationDate: 33570923, mood: "Bad"})
-                .expect(200)
-                .expect('Content-Type', /json/)
-                .then((response: Response) => {
-                    expect((response.body as ResponseObject<RunResult>).data.value.changes).toBe(1);
-                    newID = (response.body as ResponseObject<RunResult>).data.value.lastInsertRowid;
-                })
-        })
+            it("should create new note item", async () => {
+                await requestWithSuperTest
+                    .post("/progress")
+                    .send({creationDate: 33570923, mood: "Bad"})
+                    .expect(200)
+                    .expect('Content-Type', /json/)
+                    .then((response: Response) => {
+                        expect((response.body as ResponseObject<RunResult>).data.value.changes).toBe(1);
+                        newID = (response.body as ResponseObject<RunResult>).data.value.lastInsertRowid;
+                    })
+            })
 
-        it(`should return progress item with id`, async () => {
-            expect(newID).toBeGreaterThan(0);
+            it(`should return progress item with id`, async () => {
+                expect(newID).toBeGreaterThan(0);
+                await requestWithSuperTest
+                    .get(`/progress/${newID}`)
+                    .expect(200)
+                    .expect('Content-Type', /json/)
+                    .then((response: Response) => {
+                        expect(response).toBeDefined();
+                        expect((response.body as ResponseObject<ProgressItem>).data).toBeDefined();
+                        expect((response.body as ResponseObject<ProgressItem>).data.value).toBeDefined();
+                        expect((response.body as ResponseObject<ProgressItem>).data.value.id).toBe(newID);
+                    })
+            });
+
+            it(`should update mode of item`, async () => {
+                await requestWithSuperTest
+                    .patch(`/progress/${newID}/mood`)
+                    .send({mood: "Good", id: newID})
+                    .expect(200)
+                    .expect('Content-Type', /json/)
+                    .then(async (response: Response) => {
+                        expect((response.body as ResponseObject<RunResult>).data.value.changes).toBe(1);
+                        await requestWithSuperTest
+                            .get(`/progress/${newID}`)
+                            .expect(200)
+                            .expect('Content-Type', /json/)
+                            .then((getResponse: Response) => {
+                                expect((getResponse.body as ResponseObject<ProgressItem>).data.value.mood).toBe("Good");
+                            })
+                    })
+            })
+
+            it(`should delete item`, async () => {
+                await requestWithSuperTest
+                    .delete(`/progress/${newID}`)
+                    .expect(200)
+                    .expect('Content-Type', /json/)
+                    .then(async (response: Response) => {
+                        expect((response.body as ResponseObject<RunResult>).data.value.changes).toBe(1);
+                        await requestWithSuperTest
+                            .get(`/progress/${newID}`)
+                            .expect(404)
+                            .expect('Content-Type', /json/)
+                            .then((afterDelResponse: Response) => {
+                                console.log("Resp: ", afterDelResponse.body);
+                                console.log("NewID: ", newID);
+                            })
+                    })
+            })
+        });
+
+        it.skip("should return the project object with the id 1", async () => {
             await requestWithSuperTest
-                .get(`/progress/${newID}`)
+                .get("/progress/1")
                 .expect(200)
                 .expect('Content-Type', /json/)
                 .then((response: Response) => {
                     expect(response).toBeDefined();
                     expect((response.body as ResponseObject<ProgressItem>).data).toBeDefined();
-                    expect((response.body as ResponseObject<ProgressItem>).data.value).toBeDefined();
-                    expect((response.body as ResponseObject<ProgressItem>).data.value.id).toBe(newID);
-                })
+                    expect((response.body as ResponseObject<ProgressItem>).data.value.id).toBeDefined();
+                });
         });
-
-        it(`should update mode of item`, async () => {
-            await requestWithSuperTest
-                .patch(`/progress/${newID}/mood`)
-                .send({mood: "Good", id: newID})
-                .expect(200)
-                .expect('Content-Type', /json/)
-                .then(async (response: Response) => {
-                    expect((response.body as ResponseObject<RunResult>).data.value.changes).toBe(1);
-                    await requestWithSuperTest
-                        .get(`/progress/${newID}`)
-                        .expect(200)
-                        .expect('Content-Type', /json/)
-                        .then((getResponse: Response) => {
-                            expect((getResponse.body as ResponseObject<ProgressItem>).data.value.mood).toBe("Good");
-                        })
-                })
-        })
-
-        it(`should delete item`, async () => {
-            await requestWithSuperTest
-                .delete(`/progress/${newID}`)
-                .expect(200)
-                .expect('Content-Type', /json/)
-                .then(async (response: Response) => {
-                    expect((response.body as ResponseObject<RunResult>).data.value.changes).toBe(1);
-                    await requestWithSuperTest
-                        .get(`/progress/${newID}`)
-                        .expect(404)
-                        .expect('Content-Type', /json/)
-                        .then((afterDelResponse: Response) => {
-                            console.log("Resp: ", afterDelResponse.body);
-                            console.log("NewID: ", newID);
-                        })
-                })
-        })
-    });
-
-    it.skip("should return the project object with the id 1", async () => {
-        await requestWithSuperTest
-            .get("/progress/1")
-            .expect(200)
-            .expect('Content-Type', /json/)
-            .then((response: Response) => {
-                expect(response).toBeDefined();
-                expect((response.body as ResponseObject<ProgressItem>).data).toBeDefined();
-                expect((response.body as ResponseObject<ProgressItem>).data.value.id).toBeDefined();
-            });
     });
 });
